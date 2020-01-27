df1ac6dee9d1dc49eac9d8691f60bafcde0967bb
Aaditya Joshi <aadityajoshi2699@gmail.com>
2020-01-27 11:16:49 +0530

    Comments Fixed

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -9,7 +9,7 @@ from django.views.generic import (TemplateView, ListView, DetailView,
                                   CreateView, UpdateView, DeleteView)
 # Create your views here.
 
-    
+
 class AboutView(TemplateView):
     template_name = 'about.html'
 
@@ -68,18 +68,18 @@ def post_publish(request, pk):
 @login_required
 def add_comment_to_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    if request.method == 'POST':
+    if request.method == "POST":
         form = CommentForm(request.POST)
         if form.is_valid():
             comment = form.save(commit=False)
             comment.post = post
             comment.save()
             return redirect('post_detail', pk=post.pk)
-        else:
-            form = CommentForm()
-        return render(request, 'blog/comment_form.html', {'form': form})
+    else:
+        form = CommentForm()
+    return render(request, 'blog/comment_form.html', {'form': form})
 
-
+    
 @login_required
 def comment_approve(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
